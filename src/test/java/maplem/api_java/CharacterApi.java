3d574d779a1a56b990e11f8a750702ce81a713f3
minki-jeon <minki.jeon.study@gmail.com>
2025-03-27 19:54:24 +0900

    CharacterBasic Param 수정

--- src/test/java/maplem/api_java/CharacterApi.java
+++ src/test/java/maplem/api_java/CharacterApi.java
@@ -13,21 +13,21 @@ public class CharacterApi {
 	private static final String apiKey = System.getProperty("API_KEY");		// [Run Configurations > VM arguments]
 	private static final MapleStoryMApi api = new MapleStoryMApi(apiKey);	// main
 	private static final String ocid = "9aa8425eb305ff231a632c6507ab63ccc583003eb0448898768cf5ad982a332b";		// INPUT_OCID
-	
+
 	@Test
 	@DisplayName("캐릭터 식별자(ocid) 조회")
 	void getCharacter() throws IOException {
-		String nickname = "쩌밍";
-		String world = "루나";
+		String nickname = "쩌밍";				// INPUT_NICKNAME
+		String world = "루나";				// INPUT_WORLDNAME
 		CharacterDTO response = api.getCharacter(nickname, world);
 		System.out.println(response.toString());
 	}
-//	
-//	@Test
-//	@DisplayName("기본 정보 조회")
-//	void getCharacterBasic() {
-//		CharacterBasicDTO response = api.getCharaterBasic(ocid);
-//		System.out.println(response.toString());
-//		System.out.println(response.getCharacterName());
-//	}
+	
+	@Test
+	@DisplayName("기본 정보 조회")
+	void getCharacterBasic() {
+		CharacterBasicDTO response = api.getCharaterBasic(ocid);
+		System.out.println(response.toString());
+		System.out.println(response.getCharacterName());
+	}
 }
