b05de594af6ac89348a18de3cf3d137dec8e3eb4
minki-jeon <minki.jeon.study@gmail.com>
2025-04-04 20:02:56 +0900

    공지정보조회 API 호출 템플릿 추가

--- src/main/java/maplem/api_java/templete/NoticeApi.java
+++ src/main/java/maplem/api_java/templete/NoticeApi.java
@@ -1,11 +1,34 @@
 package maplem.api_java.templete;
 
+import maplem.api_java.dto.notice.EventNoticeDetailDTO;
+import maplem.api_java.dto.notice.EventNoticeListDTO;
+import maplem.api_java.dto.notice.NoticeDetailDTO;
 import maplem.api_java.dto.notice.NoticeListDTO;
+import maplem.api_java.dto.notice.PatchNoticeDetailDTO;
+import maplem.api_java.dto.notice.PatchNoticeListDTO;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 public interface NoticeApi {
 	@GET("maplestorym/v1/notice")
 	Call<NoticeListDTO> getNoticeList(@Header("x-nxopen-api-key") String apiKey);
+	
+	@GET("maplestorym/v1/notice/detail")
+	Call<NoticeDetailDTO> getNoticeDetail(@Header("x-nxopen-api-key") String apiKey, @Query("notice_id") int noticeId);
+	
+	@GET("maplestorym/v1/notice-patch")
+	Call<PatchNoticeListDTO> getPatchNoticeList(@Header("x-nxopen-api-key") String apiKey);
+	
+	@GET("maplestorym/v1/notice-patch/detail")
+	Call<PatchNoticeDetailDTO> getPatchNoticeDetail(@Header("x-nxopen-api-key") String apiKey, @Query("notice_id") int noticeId);
+	
+	@GET("maplestorym/v1/notice-event")
+	Call<EventNoticeListDTO> getEventNoticeList(@Header("x-nxopen-api-key") String apiKey);
+	
+	@GET("maplestorym/v1/notice-event/detail")
+	Call<EventNoticeDetailDTO> getEventNoticeDetail(@Header("x-nxopen-api-key") String apiKey, @Query("notice_id") int noticeId);
+	
+	
 }
